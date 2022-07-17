d0cf3b88cec9f1dacc794a3326c8436160a839d1
Jesse Cooper <CLJcareer@outlook.com>
2022-07-17 18:40:12 -0500

    Initial setup for the problem

---
+++ src/algorithm/MaximumLengthSubarrayPositiveProduct.ts
@@ -0,0 +1,11 @@
+class MaximumLengthSubarrayPositiveProduct {
+
+    constructor() {
+    }
+
+    public find(nums: number[]): number{
+        return 0;
+    }
+}
+
+export default MaximumLengthSubarrayPositiveProduct;
---
+++ src/test/MaximumLengthSubarrayPositiveProduct.test.ts
@@ -0,0 +1,23 @@
+import {describe, it} from "mocha";
+import {assert} from "chai";
+import ReverseSubstrings from "../algorithm/ReverseSubstrings";
+
+//
+//Given an array of integers nums, find the maximum length of a
+//subarray where the product of all its elements is positive.
+//
+//A subarray of an array is a consecutive sequence of zero or more values
+//taken out of that array.
+//
+//Return the maximum length of a subarray with positive product.
+//
+//
+//https://leetcode.com/problems/maximum-length-of-subarray-with-positive-product/
+//
+
+describe('', () => {
+
+    it('', () => {
+
+    })
+})
